41f9ce11c9c15c2a12f2e344a9980fbf3d824511
PProvost <peter@provost.org>
2010-01-04 13:29:19 -0700

    Cleanup code a bit before pushing to weechat scripts

--- highlight.lua
+++ highlight.lua
@@ -1,5 +1,4 @@
 --[[
-
 Highlight.lua - Simplified management of the highlights list for weechat
   Version 0.1
 
@@ -11,7 +10,6 @@ Installation:
 	Reload the lua plugin by typing '/lua reload'
 
 Usage:
-
   /highlight [list]
 	  Prints the current highlight list to the core buffer
 
@@ -27,20 +25,20 @@ Usage:
 	  Removes the <phrase> from the highlights list. See the add command
 		for details around case sensitivity.
 
-Copyright 2009 Peter Provost (PProvost@freenode)
+License:
+	Copyright 2009 Peter Provost (PProvost@freenode)
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
 ]]
 
 --
